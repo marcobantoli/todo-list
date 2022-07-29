8bdb374ce8800483867dce628ba8dd68e084acc8
Marco Bantolino <marcobantoli@gmail.com>
2022-07-29 01:08:31 -0400

    Create and read todo operations finished

--- server/index.js
+++ server/index.js
@@ -4,16 +4,30 @@ const pool = require('./db.js');
 const app = express();
 const PORT = 3001;
 
-const query = async() => {
-  const todos = await pool.query('SELECT * FROM todo');
-  console.log(todos);
-};
-
-query();
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// Create
+app.post('/todos', async (req, res) => {
+  try {
+    const description = req.body.description;
+    const newTodo = await pool.query('INSERT INTO todo(todo_id, description) VALUES(DEFAULT, $1)', [description]);
+    res.json(newTodo);
+  } catch (err) {
+    console.log(err);
+  }
 });
+
+// Read
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await pool.query('SELECT * FROM todo');
+    res.json(todos.rows);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Update
+
+// Delete
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
